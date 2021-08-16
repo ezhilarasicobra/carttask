79739c6c30204bace8bc8d7433d385f1f85c6a9c
Ezhilarasi <tezzhilarasi@gmail.com>
2021-08-17 01:08:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Card from "./Card";
+import {useState} from 'react'
 
 function App() {
+  let productdata = [
+    { id:1,name: "Fancy Product", price: 60 },
+    {id:2, name: "Special Item", price: 80 },
+    { id:3,name: "Sale Item", price: 18 },
+    { id:4,name: "Popular Item", price: 25 },
+    {id:5, name: "Sale Item", price: 40 },
+    { id:6,name: "Fancy Product", price: 25 },
+    { id:7,name: "Special Item", price: 280 },
+    { id:8,name: "Popular Item", price: 40 },
+  ];
+  const [productlist,setproductlist]=useState(productdata)
+  const [cartlist,setCartlist]=useState([])
+  const [total,SetTotal]=useState(0)
+let buttonclick=(productobj)=>{
+  setCartlist([...cartlist,productobj])
+  SetTotal(total+productobj.price)
+}
+let removecart=(productobj)=>{
+let newcart=cartlist.filter((obj)=>obj.id!==productobj.id)
+setCartlist([...newcart])
+SetTotal(total-productobj.price)
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container mt-5 ">
+        <div className="row ">
+          <div className="col-lg-2">
+            <div>
+              <h1>CART</h1>
+              <ul className="list-group">
+                {
+                  cartlist.map((item)=>{
+                   return  <li className="list-group-item d-flex justify-content-between align-items-center">
+                 <button className="btn btn-xs btn-danger" 
+                 onClick={()=>{removecart(item)}}> X </button> {item.name} = ${item.price}
+                  <span className="badge badge-primary badge-pill">14</span>
+                </li>
+                  })
+                }
+               
+              </ul>
+              <h3>Total=${total}</h3>
+            </div>
+          </div>
+          <div className="col=lg-10">
+            <div className="row ">
+              {productlist.map((product) => {
+                return <Card data={product} cartlist={cartlist} handleclick={buttonclick} />;
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,37 @@
+import React from "react";
+const Card = (props) => {
+  return (
+    <>
+    
+     <div className="col-lg-3 mb-5">
+        <div className="card h-100">
+          <img
+            className="card-img-top"
+            src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+            alt="..."
+          />
+          <div className="card-body p-4">
+            <div className="text-center">
+              <h5 className="fw-bolder">{props.data.name}</h5>
+              ${props.data.price}
+            </div>
+          </div>
+          <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div className="text-center">
+              <button className="btn btn-outline-dark mt-auto" 
+         disabled={props.cartlist.some(obj=>obj.id===props.data.id)}
+              onClick={()=>props.handleclick(props.data)}>
+                Add to Cart
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+     </>
+ 
+   
+  );
+};
+
+export default Card;
